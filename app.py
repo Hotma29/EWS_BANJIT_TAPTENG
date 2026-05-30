1c18f70bbb83fd344868b441ed4ef1b59541db12
Hotma29 <hotmahrp19@gmail.com>
2026-05-30 23:01:20 +0700

    Refactor app.py to improve structure and clarity

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ st.markdown("""
 @st.cache_resource
 def load_smart_model():
     # Load model yang sudah dilatih murni dengan 3 fitur
-    model = joblib.load('model_banjirku.pkl')
-    le = joblib.load('label_encoderku.pkl')
+    model = joblib.load('model_banjir.pkl')
+    le = joblib.load('label_encoder.pkl')
     return model, le
 
 # --- 3. FUNGSI KIRIM TELEGRAM (KHUSUS SIMULASI) ---
