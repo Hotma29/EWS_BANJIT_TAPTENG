1a5d00c7438f066c470c45183c09418b195e14c1
Hotma29 <hotmahrp19@gmail.com>
2026-07-02 15:02:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ with tab2:
                 else:
                     status_sim = "RENDAH"
                     internal_conf = prob_rendah * 100
-                    logika = f"Analisis AI (Confidence RENDAH: {internal_conf:.1f}%)"
+                    logika = f"Analisis Random Forest (Confidence RENDAH)"
                     pesan_mitigasi = "✅ AMAN: Kondisi cuaca dan resapan air normal."
 
             st.markdown("---")
